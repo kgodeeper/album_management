4384dca324310cb4c0c11b588f18ef0d390c95c8
kgodeeper <kgodeeper@gmail.com>
2022-07-27 23:46:38 +0700

    config prettier

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require("dotenv").config({ path: "./src/configs/.env" });
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+require('dotenv').config({ path: './src/configs/.env' });
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -10,5 +10,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.listen(process.env.PORT || 2000, () => {
-  console.log(`server is running on port ${process.env.PORT}`);
+	console.log(`server is running on port ${process.env.PORT}`);
 });
+
+console.log('Hello');
+
