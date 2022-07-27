cf67d1d623b86e3b6ee5de8dff6afa48b61b36a7
kgodeeper <kgodeeper@gmail.com>
2022-07-27 15:40:46 +0700

    upload base

---
+++ app.js
@@ -0,0 +1,14 @@
+require("dotenv").config({ path: "./src/configs/.env" });
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.listen(process.env.PORT || 2000, () => {
+  console.log(`server is running on port ${process.env.PORT}`);
+});
