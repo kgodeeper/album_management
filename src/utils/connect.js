ec0e30b85df2b44524b2148c3154696f32178bb1
kgodeeper <kgodeeper@gmail.com>
2022-07-28 16:20:13 +0700

    create schema and model

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,8 +1,6 @@
 require('dotenv').config({ path: './src/configs/.env' });
 const mongoose = require('mongoose');
 
-console.log(process.env.URI);
-
 function connect() {
 	mongoose.connect(process.env.URI).catch(error => {
 		console.log(`Connect Error: ${error}`);
